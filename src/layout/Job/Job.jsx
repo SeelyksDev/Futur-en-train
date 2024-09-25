0879a2a4a8c209804bb1a209c9b97802fde2ca8d
SeelyksDev <kevincordier.dev@gmail.com>
2024-09-25 22:02:59 +0200

    Modification du header

--- src/layout/Job/Job.jsx
+++ src/layout/Job/Job.jsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import Button from "../../components/Button/Button";
 import jobsList from "../../data/jobs.json";
-import "./Job.css";
 import CarouselCard from "../../components/CarouselCard/CarouselCard";
+import rightArrow from "../../assets/svg/arrow-right-white.svg";
+import leftArrow from "../../assets/svg/arrow-left-white.svg";
+import slash from "../../assets/svg/slash.svg";
+import "./Job.css";
 
 const Job = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
     return (
         <section id="job">
             <h2 className="job-title">Un métier, une formation, un avenir</h2>
@@ -19,46 +24,11 @@ const Job = () => {
                     <h4 className="carousel-title">Métiers</h4>
                     <div className="arrows-container">
                         <button className="arrow_previous">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="19"
-                                height="14"
-                                viewBox="0 0 19 14"
-                            >
-                                <path
-                                    fill="#FFF"
-                                    fillRule="evenodd"
-                                    d="M6.96 14a.974.974 0 0 1 .685-1.658l-4.352-4.383L18.035 7.906A.97.97 0 0 1 19 6.932a.97.97 0 0 1-.967-.97l-14.724.053 4.335-4.356a.976.976 0 0 1 0-1.374.964.964 0 0 1-1.368 0l-5.993 6.02A.979.979 0 0 1 0 7v.002a.98.98 0 0 1 .282.679l5.993 6.035a.961.961 0 0 1 .685.286"
-                                ></path>
-                            </svg>
+                            <img src={leftArrow} alt="flèche droite" />
                         </button>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="9"
-                            height="20"
-                            viewBox="0 0 9 22"
-                        >
-                            <path
-                                fill="none"
-                                fillRule="nonzero"
-                                stroke="#FFF"
-                                strokeLinecap="square"
-                                d="M.5 21.389L8.5.61"
-                            ></path>
-                        </svg>
+                        <img src={slash} alt="slash" />
                         <button className="arrow_next">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="19"
-                                height="14"
-                                viewBox="0 0 19 14"
-                            >
-                                <path
-                                    fill="#FFF"
-                                    fillRule="evenodd"
-                                    d="M12.04 14a.974.974 0 0 1-.685-1.658l4.352-4.383L.965 7.906A.97.97 0 0 1 0 6.932a.97.97 0 0 1 .967-.97l14.724.053-4.335-4.356a.976.976 0 0 1 0-1.374.964.964 0 0 1 1.368 0l5.993 6.02a.979.979 0 0 1 .283.69V7a.98.98 0 0 1-.282.679l-5.993 6.035a.961.961 0 0 1-.685.286"
-                                ></path>
-                            </svg>
+                            <img src={rightArrow} alt="flèche droite" />
                         </button>
                     </div>
                 </div>
