e70116052041d99bc2330b88c882f0e926a32399
SeelyksDev <kevincordier.dev@gmail.com>
2024-09-25 12:41:06 +0200

    Avancement dans le style de la partie "Stats"

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -4,8 +4,10 @@ const Stats = ({ stats, title, subtitle }) => {
     return (
         <div className="stats">
             <h3 className="stats-number">{stats}</h3>
-            <h4 className="stats-title">{title}</h4>
-            <p className="stats-subtitle">{subtitle}</p>
+            <div className="description">
+                <h4 className="stats-title">{title}</h4>
+                <p className="stats-subtitle">{subtitle}</p>
+            </div>
         </div>
     );
 };
