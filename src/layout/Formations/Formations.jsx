7abd73c31c148dfa2ab56d6e6f84fde9bd2a5b25
SeelyksDev <kevincordier.dev@gmail.com>
2024-09-26 05:31:13 +0200

    Ajout du after en bas de page

--- src/layout/Formations/Formations.jsx
+++ src/layout/Formations/Formations.jsx
@@ -3,18 +3,20 @@ import Button from "../../components/Button/Button";
 
 const Formations = () => {
     return (
-        <section id="formations">
-            <h2 className="formations-title">
-                Formations dans toute la France
-            </h2>
-            <p className="formations-text">
-                Trouvez le centre de formation qui correspond à la zone
-                géographique de votre choix
-            </p>
-            <Button
-                text="LOCALISER UNE FORMATION"
-                style="btn-gradient locate-training"
-            />
+        <section id="formations-container">
+            <div id="formations">
+                <h2 className="formations-title">
+                    Formations dans toute la France
+                </h2>
+                <p className="formations-text">
+                    Trouvez le centre de formation qui correspond à la zone
+                    géographique de votre choix
+                </p>
+                <Button
+                    text="LOCALISER UNE FORMATION"
+                    style="btn-gradient locate-training"
+                />
+            </div>
         </section>
     );
 };
