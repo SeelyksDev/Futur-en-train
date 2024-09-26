1a355f5c5c77df2fd11d7400bede723b39f64709
SeelyksDev <kevincordier.dev@gmail.com>
2024-09-26 16:03:07 +0200

    Réajustement

--- src/layout/Informations/Informations.jsx
+++ src/layout/Informations/Informations.jsx
@@ -5,29 +5,34 @@ import "./Informations.css";
 
 const Informations = () => {
     return (
-        <section id="informations">
-            <div className="info-col-left">
-                <h2 className="informations-title">
-                    Le ferroviaire, un secteur d’avenir en pleine mutation
-                </h2>
-                <p className="informations-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nemo, tempore earum tenetur, a aliquid deleniti autem
-                    delectus numquam cumque cum possimus unde consequuntur
-                    impedit aut libero quas.
-                </p>
-                <Button text="EN SAVOIR PLUS" style="btn-gradient learn-more" />
-            </div>
-            <section id="stats-container">
-                {statsList.map((stat, index) => (
-                    <Stats
-                        key={index}
-                        stats={stat.number}
-                        title={stat.title}
-                        subtitle={stat.subtitle}
+        <section className="information-wrapper">
+            <div id="informations">
+                <div className="info-col-left">
+                    <h2 className="informations-title">
+                        Le ferroviaire, un secteur d’avenir en pleine mutation
+                    </h2>
+                    <p className="informations-text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Nemo, tempore earum tenetur, a aliquid deleniti autem
+                        delectus numquam cumque cum possimus unde consequuntur
+                        impedit aut libero quas.
+                    </p>
+                    <Button
+                        text="EN SAVOIR PLUS"
+                        style="btn-gradient learn-more"
                     />
-                ))}
-            </section>
+                </div>
+                <section id="stats-container">
+                    {statsList.map((stat, index) => (
+                        <Stats
+                            key={index}
+                            stats={stat.number}
+                            title={stat.title}
+                            subtitle={stat.subtitle}
+                        />
+                    ))}
+                </section>
+            </div>
         </section>
     );
 };
